6544a3c121af365a846251235e68a189dbba55c0
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2026-01-09 14:55:52 +0800

    Initial commit

---
+++ HealthSenseWeb/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/main.css";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="heading">Welcome to HealthSenseWeb</h1>
+      <p className="subtext">Your dashboard starts here!</p>
+    </div>
+  );
+};
+
+export default Home;
