4ff3676955d824940721ebdb2561f3c2c51fe265
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2026-01-09 14:59:21 +0800

    Update home.jsx

--- HealthSenseWeb/src/pages/home.jsx
+++ HealthSenseWeb/src/pages/home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className="container">
       <h1 className="heading">Welcome to HealthSenseWeb</h1>
+      <h1 className="heading">Brendan Bayot</h1>
       <p className="subtext">Your dashboard starts here!</p>
     </div>
   );
