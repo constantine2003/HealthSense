434c202c08252ae3055e28622f55bc37fcf6800c
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2026-01-22 22:41:14 +0800

    Update

--- HealthSenseWeb/src/pages/dashboard.jsx
+++ HealthSenseWeb/src/pages/dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/dashboard.css";
 import Navbar from "../components/navbar2";
 import { useNavigate } from "react-router-dom";
+import { FiClipboard, FiClock } from 'react-icons/fi';
 
 const Dashboard = () => {
   const user = "Daniel M. Montesclaros"; // define a user for now
@@ -18,18 +19,20 @@ const Dashboard = () => {
             <p className="description-text">Access your medical checkup results and history securely.</p>
             {/* Button Row */}
             <div className="dashboard-buttons">
-              <button
-                className="dashboard-btn"
-                onClick={() => navigate("/results")}
-              >
-                View <br></br>Results
+              <button className="dashboard-btn" onClick={() => navigate("/results")}>
+                <FiClipboard  size={100} color="#0077B6" className="btn-icon" />
+                <div className="btn-text-container">
+                  <span className="btn-label">View</span>
+                  <span className="btn-title">Results</span>
+                </div>
               </button>
               
-              <button
-                className="dashboard-btn"
-                onClick={() => navigate("/history")}
-              >
-                View <br></br>History
+              <button className="dashboard-btn" onClick={() => navigate("/history")}>
+                <FiClock size={100} color="#0077B6" className="btn-icon" />
+                <div className="btn-text-container">
+                  <span className="btn-label">View</span>
+                  <span className="btn-title">History</span>
+                </div>
               </button>
             </div>
           </div>
